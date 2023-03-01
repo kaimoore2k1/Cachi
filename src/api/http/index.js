e1c375c3d60f0e5ffe2f2fefe00e75d5d7047cca
kaimoore2k1 <92667053+kaimoore2k1@users.noreply.github.com>
2023-03-01 18:57:59 +0700

    Update http

--- src/api/http/index.js
+++ src/api/http/index.js
@@ -7,20 +7,26 @@ const http = axios.create({
     "Content-Type": "application/json",
   },
   withCredentials: false,
-  timeout: 5000
+  timeout: 5000,
 });
 
-http.interceptors.request.use((config) => {
-  config.headers.Authorization = 'Bearer Token';
-  return config;
-}, (error) => {
-  return Promise.reject(error);
-})
+http.interceptors.request.use(
+  (config) => {
+    //config.headers.Authorization = 'Bearer Token';
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
-http.interceptors.response.use((response) => {
-  return response.data
-}, (error) => {
-  return Promise.reject(error);
-})
+http.interceptors.response.use(
+  (response) => {
+    return response.data;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 export default http;
