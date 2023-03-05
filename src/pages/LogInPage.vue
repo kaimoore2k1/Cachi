f27a008dd0da8f8d314f018357b5b06135eae1a8
huutinhhzt <19520299@gm.uit.edu.vn>
2023-03-05 13:05:01 +0700

    element-login-page

--- src/pages/LogInPage.vue
+++ src/pages/LogInPage.vue
@@ -1,51 +1,81 @@
 <template>
-  <div
-    class="bg-primary window-height window-width row justify-center items-center"
-  >
+  <div class="login-background" style="padding-top: 20%">
     <div class="column">
-      <div class="row">
-        <h5 class="text-h5 text-white q-my-md">Company & Cachi</h5>
-      </div>
+      <img src="../assets/images/logo.png" class="logo" alt="" />
+
+      <q-card-section class="text-center q-pa-none">
+        <p
+          class="text-1"
+          style="color: var(--q-primary); font-size: 1.2rem; font-weight: 800"
+        >
+          Login to your Account
+        </p>
+      </q-card-section>
+
       <div class="row">
         <q-card square bordered class="q-pa-lg shadow-1">
-          <q-card-section>
+          <q-card-section style="padding: 0">
             <q-form class="q-gutter-md">
               <q-input
-                v-model="username"
-                square
                 filled
-                clearable
-                type="email"
-                :label="$t('username')"
-              />
+                v-model="text"
+                :dense="dense"
+                placeholder="Enter your email adress"
+              >
+                <template v-slot:prepend>
+                  <q-icon name="email" />
+                </template>
+              </q-input>
+
               <q-input
-                square
                 filled
-                clearable
                 type="password"
-                :label="$t('password')"
-                v-model="password"
-              />
+                :dense="dense"
+                placeholder="Enter your password"
+              >
+                <template v-slot:prepend>
+                  <q-icon name="lock" />
+                </template>
+              </q-input>
+
+              <q-card-section class="text-center q-pa-none">
+                <p class="text-grey-6">Forgot Password?</p>
+              </q-card-section>
             </q-form>
           </q-card-section>
           <q-card-actions class="q-px-md">
             <q-btn
-              unelevated
-              color="primary"
-              size="lg"
-              class="full-width"
-              :label="$t('login')"
-              @click="handleLogin"
+              style="background: #ff4646; color: #fff; width: 100%"
+              label="LOGIN"
             />
           </q-card-actions>
-          <q-card-section class="text-center q-pa-none">
-            <p class="text-grey-6">Not reigistered? Created an Account</p>
-          </q-card-section>
         </q-card>
       </div>
     </div>
   </div>
 </template>
+
+<style>
+.logo {
+  width: 30%;
+  border-radius: 30%;
+  margin: 0 auto;
+}
+
+.column {
+  align-content: center;
+}
+
+.q-pa-lg {
+  border: none;
+  box-shadow: none;
+  padding: 0;
+}
+
+.shadow-1 {
+  width: 100%;
+}
+</style>
 
 <script>
 import { defineComponent, ref } from "vue";
